553171599ae85b0e74a1ce4004ec814202068ce8
walshg3 <walshg3@go.stockton.edu>
2022-01-22 17:58:03 -0500

    Added Week1 and 2 coursework

---
+++ Week2/CatNames.java
@@ -0,0 +1,30 @@
+package Week2;
+
+
+public class CatNames {
+    static String catName = "Momo";
+    //String catName = "Appa";
+    public static void main(String[] args) {
+        String catName = "Appa";
+
+        //System.out.println(catName);
+        //CatNames cat = new CatNames();
+        //System.out.println(CatNames.catName);
+
+        // what is this?? an Object!!!
+        Access gregHouse = new Access();
+        //System.out.println(gregHouse.cat1);
+        System.out.println(gregHouse.getCat1());
+        System.out.println(gregHouse.getCat2());
+        
+
+
+
+
+
+
+    }
+
+
+
+}
